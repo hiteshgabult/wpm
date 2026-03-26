918cef0da191f2df8deacc9eae0008beb1ce1dad
webcrispy <webcrispy7@gmail.com>
2026-03-26 10:39:50 +0530

    Fix cheerio import issue

--- server/services/wpFormatter.js
+++ server/services/wpFormatter.js
@@ -1,9 +1,29 @@
-export const formatToWP = (html)=>{
- return html
- .replace(/<p>/g,'<!-- wp:paragraph --><p>')
- .replace(/<\/p>/g,'</p><!-- /wp:paragraph -->')
- .replace(/<h2>/g,'<!-- wp:heading --><h2>')
- .replace(/<\/h2>/g,'</h2><!-- /wp:heading -->')
- .replace(/<table>/g,'<!-- wp:table --><table>')
- .replace(/<\/table>/g,'</table><!-- /wp:table -->');
+// server/services/htmlCleaner.js
+
+import * as cheerio from 'cheerio';
+
+export const cleanHTML = (html) => {
+  const $ = cheerio.load(html);
+
+  // Remove all span tags but keep content
+  $('span').each((i, el) => {
+    $(el).replaceWith($(el).html());
+  });
+
+  // Remove inline styles
+  $('[style]').removeAttr('style');
+
+  // Remove empty paragraphs
+  $('p').each((i, el) => {
+    if (!$(el).text().trim()) {
+      $(el).remove();
+    }
+  });
+
+  // Table cleanup (basic)
+  $('table').each((i, table) => {
+    $(table).attr('border', '1');
+  });
+
+  return $.html();
 };
